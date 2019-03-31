9f0e7e9220a78170ff9c69a98a0341a654b2f471
jpat64 <jrp3tu@virginia.edu>
2019-03-31 12:33:45 -0400

    quick workability fixes, credits
    Thanks for all the hard work over Crunch Night! I just went in this AM to get the gameplay working without being able to skip through the entire game.

--- BardsTale/Assets/LilFuckinDoorScript.cs
+++ BardsTale/Assets/LilFuckinDoorScript.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// I'm sorry, but it's probably for the best that speedrunning the game takes more than just going to every door. 
+// I've gone a little janky here, but yer gonna have to clear the room to use the doors in it.
+// Also, if the door you're trying to enter is the door out of the second room, you need to have cast fireball once.
 public class LilFuckinDoorScript : MonoBehaviour
 {
 
@@ -22,9 +25,18 @@ public class LilFuckinDoorScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        col.gameObject.transform.position = spawn.transform.position;
-        GameObject camera = GameObject.Find("Main Camera");
-        camera.transform.position = cameraPoint.transform.position;
-        static_information.room_index = static_information.which_room_am_I_in(spawn.transform.position.x, spawn.transform.position.y);
+        // to leave, the room must be clear
+        if (static_information.is_the_current_room_clear())
+        {
+            // can't leave room 2 until fireball has been cast
+            if (spawn.name.Equals("Room3Enter") && static_information.has_casted_fireball == false)
+            { return; }
+
+            // move room implementation
+            col.gameObject.transform.position = spawn.transform.position;
+            GameObject camera = GameObject.Find("Main Camera");
+            camera.transform.position = cameraPoint.transform.position;
+            static_information.room_index = static_information.which_room_am_I_in(spawn.transform.position.x, spawn.transform.position.y);
+        }
     }
 }

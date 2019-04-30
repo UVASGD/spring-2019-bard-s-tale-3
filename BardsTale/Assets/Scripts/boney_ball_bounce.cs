dd8cdb52789e3fac4e0bd4247d856923e6092267
Kevin Li <kl7ck@virginia.edu>
2019-04-30 13:24:29 -0400

    enlarged boneyball and now it deals damage

--- BardsTale/Assets/Scripts/boney_ball_bounce.cs
+++ BardsTale/Assets/Scripts/boney_ball_bounce.cs
@@ -5,6 +5,7 @@ public class boney_ball_bounce : MonoBehaviour
 {
     public int collide_count = 0;
     public GameObject Skeleton;
+    public float damage_cooldown = 0;
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col != null && col.gameObject.tag == "Room")
@@ -17,6 +18,24 @@ public class boney_ball_bounce : MonoBehaviour
                 skelly.GetComponent<SpriteRenderer>().sortingLayerName = "Moreground";
                 Destroy(this.gameObject);
             }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Hero")
+        {
+            // skeleton will attack if it is within 0.1 of the hero
+            if (damage_cooldown > 0)
+            {
+                damage_cooldown--;
+            }
+            else
+            {
+                static_information.hero.GetComponent<hero_act>().takeDamage();
+                damage_cooldown = 1;
+            }
+
+            static_information.heroRigidBody.AddForce((static_information.hero.transform.position - transform.position).normalized * 250f);
         }
     }
 }
